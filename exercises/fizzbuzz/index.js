6a28736b85efdf4b361de8fcef40ca56dad21cd2
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-14 23:34:23 -0400

    Update index.js

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -28,3 +28,7 @@ module.exports = fizzBuzz;
 //     var f = i % 3 == 0, b = i % 5 == 0;
 //     console.log(f ? b ? "FizzBuzz" : "Fizz" : b ? "Buzz" : i);
 //   }
+
+// First, we create a for-loop that will count from 1 to 100.
+// for (let i = 1; i <= 100; i++) {
+// }
