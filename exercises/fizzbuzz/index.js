ec9200688fb4d6c9f9d0002d7162defd94d3ae0f
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-14 23:32:13 -0400

    Update index.js

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -13,5 +13,12 @@
 //   buzz
 
 function fizzBuzz(n) {}
-
+if (i % 15 == 0)
+console.log("FizzBuzz");
+else if (i % 3 == 0)
+console.log("Fizz");
+else if (i % 5 == 0)
+console.log("Buzz");
+else
+console.log(i);
 module.exports = fizzBuzz;
